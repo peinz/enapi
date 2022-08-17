8f534a5c00426e992a0a9ee5e321e5ce87f2266c
peinz <philippHeinz66@gmail.com>
2022-08-17 15:31:53 +0200

    decouple request handling from server impl

--- api-lib.ts
+++ api-lib.ts
@@ -171,6 +171,88 @@ type APIClient<Tendpoints extends { [key: string]: Endpoint<any, any> }> =
         };
   }>;
 
+export const RequestHandler = <Tendpoints extends Endpoints>(
+  endpoints: Tendpoints,
+) => {
+  const routes = Object.keys(endpoints);
+  const makeResult = (statusCode: number, body: Record<string, any>) => ({
+    statusCode,
+    body,
+  });
+
+  return {
+    handle: (request: {
+      method: "GET" | "POST" | "PATCH" | "DELETE";
+      url: string;
+      body?: Record<string, any>;
+      queryParams: Record<string, string>;
+    }) => {
+      const { method, url, body, queryParams } = request;
+      const entity_route_match = (() => {
+        const isDig = "[0|1|2|3|4|5|6|7|8|9]"; // \d did not work; deno bug???
+        const regep = new RegExp(`^/([^/]+)/(${isDig}+)$`);
+        const m = regep.exec(url);
+        if (!m) return undefined;
+        const route = m[1];
+        if (!routes.includes(route)) return undefined;
+        return {
+          route: route as keyof Tendpoints,
+          id: parseInt(m[2]),
+        };
+      })();
+
+      const collection_route_match = (() => {
+        const regep = new RegExp(`^/([^/]+)/?$`);
+        const m = regep.exec(url);
+        if (!m) return undefined;
+        const route = m[1];
+        if (!routes.includes(route)) return undefined;
+        return route as keyof Tendpoints;
+      })();
+
+      if (method === "GET" && entity_route_match) {
+        const ep = endpoints[entity_route_match.route];
+        const result = ep.implementation.get(entity_route_match.id);
+        return result
+          ? makeResult(200, result)
+          : makeResult(404, { err: "not found" });
+      }
+
+      if (method === "POST" && collection_route_match) {
+        const ep = endpoints[collection_route_match];
+        if (!ep.implementation.post) return undefined;
+        if (!body) return makeResult(400, { err: "body missing" });
+        const result = ep.implementation.post(body);
+        return makeResult(201, result);
+      }
+
+      if (method === "PATCH" && entity_route_match) {
+        const ep = endpoints[entity_route_match.route];
+        if (!ep.implementation.patch) return undefined;
+        if (!body) return makeResult(400, { err: "body missing" });
+        const result = ep.implementation.patch(entity_route_match.id, body);
+        return result
+          ? makeResult(200, result)
+          : makeResult(404, { err: "not found" });
+      }
+
+      if (method === "DELETE" && entity_route_match) {
+        const ep = endpoints[entity_route_match.route];
+        if (!ep.implementation.delete) return undefined;
+        ep.implementation.delete(entity_route_match.id);
+        return makeResult(204, { status: "success" });
+      }
+
+      if (method === "GET" && collection_route_match) {
+        const ep = endpoints[collection_route_match];
+        if (!ep.implementation.getCollection) return undefined;
+        const result = ep.implementation.getCollection(queryParams);
+        return makeResult(200, result);
+      }
+    },
+  };
+};
+
 export const ApiClient = <Tendpoints extends Endpoints>(
   endpoints: Tendpoints,
 ): APIClient<Tendpoints> => {
--- server.ts
+++ server.ts
@@ -1,103 +1,35 @@
 #!/usr/bin/env -S deno run --allow-read --allow-net --watch
 
-import {
-  json,
-  NextFunction,
-  opine,
-  OpineRequest,
-  OpineResponse,
-} from "https://deno.land/x/opine@2.0.0/mod.ts";
+import { json, opine } from "https://deno.land/x/opine@2.0.0/mod.ts";
 import { opineCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { endpoints } from "./api.ts";
 import { createOpenApiJsonDoc } from "./open-api-docs.ts";
-import { Endpoint } from "./api-lib.ts";
+import { RequestHandler } from "./api-lib.ts";
 
-const app = opine();
-app.use(opineCors());
-app.use(json());
+const requestHandler = RequestHandler(endpoints);
 
-app.use((
-  req: OpineRequest<any, any, any>,
-  res: OpineResponse<any>,
-  next: NextFunction,
-) => {
-  const base_url = req.url.split("?")[0];
-  const routes = Object.keys(endpoints);
+const server = opine();
+server.use(opineCors());
+server.use(json());
 
-  const entity_route_match = (() => {
-    const isDig = "[0|1|2|3|4|5|6|7|8|9]"; // \d did not work; deno bug???
-    const regep = new RegExp(`^/([^/]+)/(${isDig}+)$`);
-    const m = regep.exec(base_url);
-    if (!m) return undefined;
-    const route = m[1];
-    if (!routes.includes(route)) return undefined;
-    return {
-      route: route as keyof (typeof endpoints),
-      id: parseInt(m[2]),
-    };
-  })();
+server.use((req, res, next) => {
+  const result = requestHandler.handle({
+    method: req.method as any,
+    url: req.url.split("?")[0],
+    body: req.parsedBody,
+    queryParams: req.params,
+  });
 
-  const collection_route_match = (() => {
-    const regep = new RegExp(`^/([^/]+)/?$`);
-    const m = regep.exec(base_url);
-    if (!m) return undefined;
-    const route = m[1];
-    if (!routes.includes(route)) return undefined;
-    return route as keyof (typeof endpoints);
-  })();
-
-  if (req.method === "GET" && entity_route_match) {
-    const ep = endpoints[entity_route_match.route];
-    const result = ep.implementation.get(entity_route_match.id);
-    return result
-      ? res.json(result)
-      : res.setStatus(404).json({ err: "not found" });
-  }
-
-  if (req.method === "POST" && collection_route_match) {
-    const ep = endpoints[collection_route_match] as Endpoint<any, any>;
-    if (!ep.implementation.post) return next();
-    const body = req.parsedBody;
-    if (!body) return res.setStatus(400).json({ err: "body missing" });
-    const result = ep.implementation.post(body);
-    return res.setStatus(201).json(result);
-  }
-
-  if (req.method === "PATCH" && entity_route_match) {
-    const ep = endpoints[entity_route_match.route] as Endpoint<any, any>;
-    if (!ep.implementation.patch) return next();
-    const body = req.parsedBody;
-    if (!body) return res.setStatus(400).json({ err: "body missing" });
-    const result = ep.implementation.patch(entity_route_match.id, body);
-    return result
-      ? res.json(result)
-      : res.setStatus(404).json({ err: "not found" });
-  }
-
-  if (req.method === "DELETE" && entity_route_match) {
-    const ep = endpoints[entity_route_match.route] as Endpoint<any, any>;
-    if (!ep.implementation.delete) return next();
-    ep.implementation.delete(entity_route_match.id);
-    return res.setStatus(204).end();
-  }
-
-  if (req.method === "GET" && collection_route_match) {
-    const ep = endpoints[collection_route_match];
-    if (!ep.implementation.getCollection) return next();
-    const queryParams = req.query;
-    const result = ep.implementation.getCollection(queryParams);
-    return res.json(result);
-  }
-
-  next();
+  if (!result) next();
+  else res.setStatus(result.statusCode).json(result.body);
 });
 
-app.get("/_docs.openapi", (_req, res) => {
+server.get("/_docs.openapi", (_req, res) => {
   const doc = createOpenApiJsonDoc(endpoints);
   res.json(doc);
 });
 
-app.listen(
+server.listen(
   3000,
   () => console.log("server has started on http://localhost:3000 🚀"),
 );
